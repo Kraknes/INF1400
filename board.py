af497adab67bb22c3d59624b3661befb51cc8fd6
Kraknes <49072404+Kraknes@users.noreply.github.com>
2025-01-15 22:22:49 +0100

    Add files via upload

--- board.py
+++ board.py
@@ -1,4 +1,7 @@
 from sudoku_reader import Sudoku_reader
+import time
+
+#test
 
 class Board:
     # It is your task to subclass this in order to make it more fit
@@ -9,35 +12,53 @@ class Board:
         self.n_rows = len(nums[0]) # = 9   
         self.n_cols = len(nums) # = 9
         self.nums = nums
-        # self.sudoku må super() hentes til subclass sudoku
-
-    
-
-    def _set_up_nums(self):
-        liste = []
-        # Set up the squares on the board (ints into Square objects)
-        for i in range(self.n_cols):
-            for j in range (self.n_rows):
-                s = Square() 
-                s.number = self.nums[i][j]
-                liste.append(s)
-
-        return liste
         
-        # Lager squares her med nummer fra en sudoku. Skal lage 81 classe squares
-        # Hvordan hente man nummeran? 
+    def _set_up_nums(self):
+        liste = []                                  # Set up the squares on the board (ints into Square objects)
+        for i in range(self.n_cols):
+            for j in range(self.n_rows):           # Lager squares her med nummer fra en sudoku. Skal lage 81 classe squares
+                s = Square(self) 
+                s.number = self.nums[i][j]
+                self.nums[i][j] = s
+                liste.append(s)                     # Lager en liste over alle squares som returneres til Sudoku class
+        return liste                        
 
     def _set_up_elems(self):
         # You should set up links between your squares and elements
         # (rows, columns, boxes)
-        # Henter squares fra _set_up_nums og legger dem til i riktig element. 
-        
-        pass    
+        for _ in range(9):                                                                         # Lager rad, kolonne og box elementer
+            self.row_list.append(element())
+            self.column_list.append(element())
+            self.box_list.append(element())
+            
+        for i in range(len(self.square_lists)):     # Går gjennom alle squares i square_list og referer til kolonne, rad og box.
+            s = self.square_lists[i] 
+            s.column = self.column_list[i%9]
+            s.row = self.row_list[int(i/9)]
+            s.box = self.box_list[int((i%9)/3)+(int((i/9)/3)*3)]
+            s.column.squaresinElement.append(s)
+            s.row.squaresinElement.append(s)
+            s.box.squaresinElement.append(s)
+            
+        for j in range(9):                           # Går gjennom squares og lager liste over ulovlige tall i elementene:
+            self.row_list[j].illegal_numbers()
+            self.column_list[j].illegal_numbers()
+            self.box_list[j].illegal_numbers()
 
     def solve(self):
         # Your solving algorithm goes here!
-        pass
-
+        self.solved = True
+        for square in self.square_lists:
+            if square.number != 0:
+                continue
+            else:
+                square.checking_legal_nr()
+                if square.number == 0:
+                    self.solved = False
+                    break
+                else:
+                    continue
+     
     # Makes it possible to print a board in a sensible format
     def __str__(self):
         r = "Board with " + str(self.n_rows) + " rows and " + str(self.n_cols) + " columns:\n"
@@ -50,62 +71,91 @@ class Board:
         return r
 
 class Sudokuboard(Board):
-    def __init__(self, square_lists):
-        super().__init__(self)
-        self.square_lists = super(board)._set_up_nums()
-                         
-    # Henter inn solve funksjon fra board. I teorien skal board classe funke til alle brettspill, så den må være generell
-        pass
-
+    def __init__(self, nums):
+        super().__init__(nums)                                                                     # Arver init av Board klassen
+        self.solved = 0
+        self.used_squares, self.row_list, self.column_list, self.box_list = [], [], [], []         # Lager tomme element lister
+        self.square_lists = super()._set_up_nums()                                                 # Lager squares av sudoku brettet
+        super()._set_up_elems()                     # Setter opp respektive elementer til hver square
+        super().__str__()
+        while self.solved == 0:                      
+            super().solve()
+        
 class Square:
-    def __init__(self):
+    def __init__(self, sudoku):
         self.number = None
-        self.mapped = False
         self.row = None
         self.column = None
         self.box = None
+        self.legal_numbers = [1,2,3,4,5,6,7,8,9]
+        self.sudoku = sudoku
+        self.iterator = 0
          
+    def checking_legal_nr(self):
+        self.legal_numbers = [item for item in self.legal_numbers if item not in self.row.illegal_list]     # Fjerner alle ulovlige tall i lovlige tall listen til square
+        self.legal_numbers = [item for item in self.legal_numbers if item not in self.column.illegal_list]
+        self.legal_numbers = [item for item in self.legal_numbers if item not in self.box.illegal_list]
+        if len(self.legal_numbers) == 0:
+            self.backtrack()
+        else:
+            self.insert_number()
 
-    def legal_numbers(self, element):
-        for value in element:
-            if self.mapped == True:
-                break
-            if value in self.legal_numbers:
-                self.legal_numbers.remove(value)
+    def insert_number(self):
+        self.number = self.legal_numbers[self.iterator]
+        self.sudoku.used_squares.append(self)
+        self.add_illegal_nr()
+    
+    def add_illegal_nr(self):
+        self.row.illegal_list.append(self.number)
+        self.column.illegal_list.append(self.number)
+        self.box.illegal_list.append(self.number)
+        
+    def backtrack(self):
+        self.sudoku.used_squares.append(self)
+        for s in reversed(self.sudoku.used_squares):
+            if len(s.legal_numbers) == 0:                                   # Square som ikke hadde noen løsninger
+                s.iterator_reset()
+                s.legal_numbers = [1,2,3,4,5,6,7,8,9] 
+            else:
+                s.row.illegal_list.remove(s.number)
+                s.column.illegal_list.remove(s.number)
+                s.box.illegal_list.remove(s.number)
+                s.number = 0
+                s.legal_numbers = [1,2,3,4,5,6,7,8,9]
+        self.sudoku.used_squares = []        
 
-            
-    def insert_number(self,value):
-        self.number = value
-            
-
+    def iterator_reset(self):            # Iterator som går gjennom alle tall i legal_numbers hvis det blir backtracking
+        if self.iterator == (len(self.legal_numbers)):
+            self.iterator = 0
+            self_nr_in_list = self.sudoku.used_squares.index(self)            # finner seg i used_squares (iter nummer), endrer til neste tall i listen av legal_numbers til forrige square 
+            prev_square = self.sudoku.used_squares[self_nr_in_list-1]            # Hvis en square har brukt alle sine muligheter så endrer iteratoren på den som kom før
+            prev_square.iterator += 1
+            prev_square.iterator_reset()
+    
+    def __str__(self):
+        return str(self.number)
+        
 class element:
-    def __init__(self, square_list, element_name):
-        self.squares = square_list
-        self.illegal_number = []
-        self.legal_numbers = [1,2,3,4,5,6,7,8,9]   
-        self.element_name = element_name
-        self.making_element()
+    def __init__(self):
+        self.squaresinElement = []
+        self.illegal_list = []   
         
     ## Lager element, enten rad, kolonne eller box av en liste på 9 class square. lager en egen liste for "illegal numbers" som ikke kan endres. Har "navn" på hva hvilke elemeent det er. 
-    def making_element(self, element_name):
-        for square in self.squares:
-            if square.number is None:
-                square.number = 0
+    def illegal_numbers(self):
+        for square in self.squaresinElement:
             if square.number != 0:
-                self.illegal_number.append(square.number)
+                self.illegal_list.append(square.number)
             
-        
-        
-    
-    
-
-        
-
 if __name__ == "__main__":
-    # Test code...
-    reader = Sudoku_reader("sudoku_10.csv")
-    # kanskje endre denne til sudokuo istedenfor board? 
-    board = Board(reader.next_board())
-    liste = board._set_up_nums()
-    
-    # print(board)
+    start = time.time()
+    sudoku_problems = "sudoku_1M.csv"
+    reader = Sudoku_reader(sudoku_problems)
+    n = 1000 
+    print("\nStart of solving", n ,"sudoku problems:\n")
+    for _ in range(n):
+        board = Sudokuboard(reader.next_board())
+        # print(board)
+    end = time.time()
+    length = end - start
+    print("\nThis algorithm used", length, "seconds solving", n, "sudoku problems!\n")
+
